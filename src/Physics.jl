963bd894ca61f4e4be7ce033e29b486bcd90d884
Soham Mukherjee <soham2446@gmail.com>
2018-01-14 09:10:18 -0500

    Adding CartesianIndex to Physics.jl

--- src/Physics.jl
+++ src/Physics.jl
@@ -3,47 +3,44 @@
 # Soham 01-2018
 #--------------------------------------------------------------------
 
-function operator{T<:Integer}(N::T)	
-	# operator = eye((N+1)^2, (N+1)^2)
-	operator = eye(N+1, N+1, N+1, N+1)
-	# Take care of the order!
-	for i in 1:N+1, j in 1:N+1, k in 1:N+1, l in 1:N+1
-		if 	!(i==1 || k==1)
-			# I = (N+1)*(i-1) + k
-			# J = (N+1)*(j-1) + l
-			# for m in 1:N+1, n in 1:N+1
-			# 	# TODO: Replace with appropriate functions for D and w
-			# 	# operator[I, J] += w[i]*w[k]*(delta[i,m]*D[k,n]*delta[m,j]*D[n,l] + 
-			# 	# 							 D[i,m]*delta[k,n]*delta[m,j]*D[n,k]) 
-			# 	operator[k, i, l, j] += w[i]*w[k]*(delta[i,m]*D[k,n]*D[m,j]*delta[n,l] + 
-			# 								       D[i,m]*delta[k,n]*delta[m,j]*D[n,l]) 
-			# end
-			operator[k, i, l, j] += 2*w[i]*w[k]*D[k,l]*D[i,j] 
-		end
+function operator{T<:Int}(N::T, M::T)::Array{Float64, 4}
+	operator = zeros(N+1, N+1, N+1, N+1)
+	#for k in 1:N+1, i in 1:N+1, l in 1:N+1, j in 1:N+1
+    for index in CartesianRange(size(operator))    
+        k = index.I[1]
+        i = index.I[2]
+        l = index.I[3]
+        j = index.I[4]   
+		
+        if 	i==1 || k==1
+			#operator[k, i, l, j] = delta(i,j)*delta(k,l)
+		    operator[index] = delta(i,j)*delta(k,l)
+        else
+			#operator[k, i, l, j] = (2.0/M)*chebw(i,N)*chebw(k,N)*chebd(k,l,N)*chebd(i,j,N)
+	        operator[index] = (2.0/M)*chebw(i,N)*chebw(k,N)*chebd(k,l,N)*chebd(i,j,N)	
+        end
 	end
-	operator = reshape(operator, ((N+1)^2, (N+1)^2))
 	return operator
 end
 
-function initializeB{T<:Integer}(N::T, loc::Array{T,1}, M::T)	
-	# b = zeros((N+1)^2)
-	b = zeros(N+1, N+1)
-	bl(x,y) = sin(pi*x)
-	br(x,y) = sin(pi*y)
-	ve(x,y) = 0
+function setB{T<:Integer}(N::T, M::T, loc::Array{T,1})::Array{Float64,2}	
+	B = zeros(N+1, N+1)
+
+    bl(x,y) = 1.0
+	br(x,y) = 1.0
+	v(x,y)  = 0.0
+
 	for i in 1:(N+1), j in 1:(N+1)
-		# I = (N+1)*(i-1) + j
-		xp, yp = coortrans([x,y], loc, M)
+		xp, yp = coortrans([chebx(i),chebx(j)], loc, M)
 		if i == 1
-			b[j,i] = bl(xp, yp)
+			B[j,i] = bl(xp, yp)
 		elseif j == 1
-			b[j,i] = br(xp, yp)
+			B[j,i] = br(xp, yp)
 		else
-			b[j,i] = w[i]*w[j]*ve(xp, yp)
+			B[j,i] = chebx(i, N)*chebx(j,N)*v(xp, yp)
 		end
 	end
-	b = reshape(b, ((N+1)^2,))
-	return b
+	return B
 end
 
 
