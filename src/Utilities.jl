90584dec4379394aceee2a7a3f09267d3de3f8d9
Soham Mukherjee <soham2446@gmail.com>
2018-02-09 15:56:19 -0500

    Adding fixes

--- src/Utilities.jl
+++ src/Utilities.jl
@@ -60,21 +60,21 @@ function chebgrid(N::Int)::Array{Float64,1}
     return Float64[chebx(i,N) for i in 1:N+1] 
 end
 
-function pconvergence(N::Int)::Float64
-    dbase = distribute(N, 1, x-> sin(pi*x), y->sin(pi*y))
+function pconvergence(N::Int, fbnd1:: Function, fbnd2::Function, fsol::Function)::Float64
+    dbase = distribute(N, 1, fbnd1, fbnd2)
     chebGrid        = Float64[chebx(i, N) for i in 1:N+1]
     chebGridData    = dbase[[1,1]]
     gaussGrid, w    = gauss(2*N)
     gaussGrid       = - gaussGrid
-    exactGridData   = Float64[sin(pi*i) + sin(pi*j) for i in gaussGrid, j in gaussGrid]
+    exactGridData   = Float64[fsol(i,j) for i in gaussGrid, j in gaussGrid]
     interpGridData  = interpolatePatch(chebGridData, gaussGrid, gaussGrid).value
     errorGridData   = interpGridData - exactGridData
     L2errorGridData = sqrt((w'*(errorGridData.^2)*w)/(w'*(exactGridData.^2)*w))
     return L2errorGridData
 end
 
-function hconvergence(N::Int, M::Int)::Float64
-    dbase = distribute(N, M, x-> sin(pi*x), y->sin(pi*y))
+function hconvergence(N::Int, M::Int, fbnd1:: Function, fbnd2::Function, fsol::Function)::Float64
+    dbase = distribute(N, M, fbnd1, fbnd2)
     chebGrid        = Float64[chebx(i, N) for i in 1:N+1]
     gaussGrid, w    = gauss(2*N)
     gaussGrid       = -gaussGrid
@@ -83,7 +83,7 @@ function hconvergence(N::Int, M::Int)::Float64
         loc             = [i,j]
         gaussLocalx     = Float64[coordtrans(M, [x, 0], loc)[1] for x in gaussGrid]
         gaussLocaly     = Float64[coordtrans(M, [0, y], loc)[2] for y in gaussGrid] 
-        exactGridData   = Float64[sin(pi*x) + sin(pi*y) for x in gaussLocalx, y in gaussLocaly]
+        exactGridData   = Float64[fsol(x,y) for x in gaussLocalx, y in gaussLocaly]
         # XXX: Check why we need to flip the array?
         interpGridData  = interpolatePatch(dbase[loc[end:-1:1]], gaussGrid, gaussGrid).value
         errorGridData   = interpGridData - exactGridData
--- test/GridTest.jl
+++ test/GridTest.jl
@@ -5,10 +5,30 @@
 
 @test 1==1
 
-for n in 3:24
-    @show n, pconvergence(n)
+#=
+fr(x)    = sin(pi*x) 
+fc(y)    = sin(pi*y)
+sol(x,y) = sin(pi*x) + sin(pi*y)
+
+errorvec = []
+for n in 3:20
+    L2errornorm = pconvergence(n, fr, fc, sol)
+    @show n, L2errornorm
+    append!(errorvec, L2errornorm)
 end
 
-for m in 2:2:24
-   @show m, hconvergence(12, m)
+# Hacky way to save the file
+#using HDF5
+#h5open("pconv.h5", "w") do file
+#    g = g_create(file, "pconvergence") # create a group
+#    g["n"]  = collect(3:20)              # create a scalar dataset inside the group
+#    g["errorvec"] = errorvec  
+#    attrs(g)["Description"] = "Test dataset" # an attribute
+#end
+
+temp = 1.0
+for m in map(x -> 2^x, 2:9)
+    error = hconvergence(1, m, fr, fc, sol)
+    temp  = error
 end
+=#
