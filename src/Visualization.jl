75981de62e01c3ab59b718b0e0ba8014ac3916fd
Soham Mukherjee <soham2446@gmail.com>
2018-03-05 21:57:40 -0500

    Visualization.jl

--- src/Visualization.jl
+++ src/Visualization.jl
@@ -3,21 +3,22 @@
 # Soham 03-2018
 #--------------------------------------------------------------------
 
-function generatecmap(patch::Array{Float64,2}, N::Int)
-    npatch = (patch .- minimum(patch))./(maximum(patch) .- minimum(patch))
-    colors = colormap("Blues", N+1)
-    return reshape(colors[vec(round.(Int, npatch*N+1))], size(npatch))
+function setcolormap(vec::Array{Float64,1}, map::String, samples::Int)
+    nvec = (vec .- minimum(vec))./(maximum(vec) .- minimum(vec))
+    clrs = colormap(map, samples+1)
+    return clrs[round.(Int, (nvec*samples)+1)]
 end
 
-function drawpatch(patch::Array{Float64, 2})
-
+function drawpatch(patch::Array{Float64,2})
     (Nx, Ny) = size(patch) .- 1
     (wx, wy) = (chebweights(Nx), chebweights(Ny))
     (lx, ly) = (sort(2.0 - cumsum(wx))*200, sort(2.0 - cumsum(wy))*200)
-    cmap = generatecmap(patch, 100000)
-    canvas = Drawing(800, 800, "plotpatch.pdf")
+    cmap     = reshape(setcolormap(vec(patch), "Blues", 100000), size(patch))
+    canvas   = Drawing(800, 800, "luxor-patch.pdf")
 
+    #-----------------------------------------------
     # draw patch
+    #-----------------------------------------------
     origin(400, 650)
     rotate(-3pi/4)
     setline(0.4)
@@ -26,7 +27,9 @@ function drawpatch(patch::Array{Float64, 2})
        rect(lx[i], ly[j], lx[i+1] - lx[i], ly[j+1] - ly[j], :fill)
     end
 
+    #-----------------------------------------------
     # set ticks
+    #-----------------------------------------------
     sethue("black")
     setline(0.4)
     (lx0, ly0) = (lx[2], ly[2])
@@ -57,30 +60,26 @@ function drawpatch(patch::Array{Float64, 2})
     line(Point(lxe, ly0), Point(lxe, lye), :stroke)
     line(Point(lx0, lye), Point(lxe, lye), :stroke)
 
+    #-----------------------------------------------
     # tick labels
+    #-----------------------------------------------
     settext("(1, 1)", Point(lx0 - 18, ly0 - 18);
             halign = "center",
-            valign = "top",
-            markup = false)
-
+            valign = "top")
     settext("(-1, -1)", Point(lxe + 18, lye + 18);
             halign = "center",
-            valign = "bottom",
-            markup = false)
+            valign = "bottom")
     settext("(-1, 1)", Point(lx0 - 20, lye + 20);
             halign = "center",
-            valign = "center",
-            markup = false)
+            valign = "center")
     settext("(1, -1)", Point(lxe + 20, ly0 - 20);
             halign = "center",
-            valign = "center",
-            markup = false)
+            valign = "center")
 
+    #-----------------------------------------------
     # colorbar
+    #-----------------------------------------------
     rotate(3pi/4)
-    # line(Point(-200, +100), Point(200, +100))
-    # strokepath()
-
     x   = collect(linspace(-200, 200, 100))
     clr = colormap("Blues", 100)
 
@@ -89,44 +88,44 @@ function drawpatch(patch::Array{Float64, 2})
         rect(x[i], 92.5, x[i+1] - x[i], 15, :fill)
     end
 
+    #-----------------------------------------------
     # colobar ticks
+    #-----------------------------------------------
     sethue("black")
     setline(0.4)
     line(Point(-200, 107.5), Point(-200, 85), :stroke)
-    line(Point(200, 107.5), Point(200, 85), :stroke)
     line(Point(0, 107.5), Point(0, 85), :stroke)
-
+    line(Point(200, 107.5), Point(200, 85), :stroke)
     min = round(Int, findmin(patch)[1])
     max = round(Int, findmax(patch)[1])
     med = round(Int, (min + max)/2)
 
     settext("$min", Point(-200, 80);
                 halign = "center",
-                valign = "center",
-                markup = false)
+                valign = "center")
     settext("$med", Point(0, 80);
                 halign = "center",
-                valign = "center",
-                markup = false)
+                valign = "center")
     settext("$max", Point(200, 80);
                 halign = "center",
-                valign = "center",
-                markup = false)
+                valign = "center")
 
+    #-----------------------------------------------
     #arrow
+    #-----------------------------------------------
+    sethue("black")
+    setline(0.4)
     origin(700, 100)
     arrow(O, O .+ Point(40, -40))
     settext("u", O .+ Point(40, -40);
                 halign = "top",
-                valign = "right",
-                markup = false)
+                valign = "right")
     rotate(-pi/2)
     arrow(O, O .+ Point(40, -40))
     settext("v", O .+ Point(40, -40);
                 halign = "bottom",
-                valign = "right",
-                angle  = 0,
-                markup = false)
+                valign = "right")
+
     finish()
     return canvas
 end
