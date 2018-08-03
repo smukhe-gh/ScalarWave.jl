c790abd061cbdc640728cae0f083ca655bf6b796
Soham Mukherjee <smukherjee@perimeterinstitute.ca>
2018-08-03 11:29:03 -0400

    Adding OperatorTest.jl

--- test/OperatorTest.jl
+++ test/OperatorTest.jl
@@ -50,10 +50,10 @@ function testderivOP(Px::Int, Py::Int)
         arrW[index]   = W(l, ll, m, mm, Px, Py) 
         arrdU[index]  = dU(l, ll, m, mm, Px) 
         arrdV[index]  = dV(l, ll, m, mm, Py) 
-        arrguu[index] = guu(l, ll, m, mm, Px, Py, 1) 
-        arrguv[index] = guv(l, ll, m, mm, Px, Py, 1) 
-        arrgvu[index] = gvu(l, ll, m, mm, Px, Py, 1) 
-        arrgvv[index] = gvv(l, ll, m, mm, Px, Py, 1) 
+        arrguu[index] = 1.0 #guu(l, ll, m, mm, Px, Py, 1) 
+        arrguv[index] = 1.0 #guv(l, ll, m, mm, Px, Py, 1) 
+        arrgvu[index] = 1.0 #gvu(l, ll, m, mm, Px, Py, 1) 
+        arrgvv[index] = 1.0 #gvv(l, ll, m, mm, Px, Py, 1) 
     end
 
     for index in CartesianRange(size(operator))
@@ -64,7 +64,7 @@ function testderivOP(Px::Int, Py::Int)
                  for ii in 1:Py+1) 
         L3 = arrW[m, ll, m, ll]*arrguv[m, ll, m, ll]*arrdU[m, ll, l, ll]*arrdV[m, ll, m, mm] +
              arrW[l, mm, l, mm]*arrgvu[l, mm, l, mm]*arrdV[l, mm, l, ll]*arrdU[l, mm, m, mm]
-        operator[index] = L1 + L2 + L3
+        operator[index] = L3 # + L1 + L2
     end
     return operator
 end
@@ -80,10 +80,55 @@ function testboundaryOP{T<:Int}(Px::T, Py::T)::Array{Float64, 4}
     return bnd
 end
 
-fH2L  = vec([x^3 + y^5 for x in chebgrid(Nx), y in chebgrid(Ny)])
-opH2L = shapeH2L(testderivOP(Nx, Ny), Nx, Ny)
-bpH2L = shapeH2L(testboundaryOP(Nx, Ny), Nx, Ny)
+function testRHS(ufunc::Function, vfunc::Function, Px::Int, Py::Int)
+    B = zeros(Px+1, Py+1)
+    u = chebgrid(Px)
+    v = chebgrid(Py)
+    B[:,1] = ufunc.(u)
+    B[1,:] = vfunc.(v)
+    return B
+end
 
-# Now test the functions of both of these operators
+Nx, Ny = 40, 40
+
+#=
+fH2L   = vec([x^3 + y^5 for x in chebgrid(Nx), y in chebgrid(Ny)])
+opH2L  = shapeH2L(testderivOP(Nx, Ny))
+L      = opH2L + bpH2L
+IntOP  = eye((Nx+1)*(Ny+1)) + diagm(b) 
 @show fH2L'*opH2L*fH2L
-@show bpH2L / fH2L
+=#
+
+@assert Nx == Ny
+bpH2L  = shapeH2L(testboundaryOP(Nx, Ny))
+b      = shapeH2L(testRHS(u->0, v->10*exp(-v^2/0.2), Nx, Ny))
+Du     = Float64[chebd(i, j, Nx) for i in 1:Nx+1, j in 1:Nx+1]
+Dv     = Float64[chebd(i, j, Ny) for i in 1:Ny+1, j in 1:Ny+1]
+Wu     = Float64[chebw(i, Nx) for i in 1:Nx+1]
+Wv     = Float64[chebw(i, Ny) for i in 1:Ny+1]
+WUV    = diagm(vec(kron(Wu, Wv)))
+DV     = kron(eye(Nx+1), Dv)
+DU     = kron(Du, eye(Ny+1))
+
+twist  = 20
+gUU    = diagm(vec(Float64[-cos((pi/twist)*cospi(chebx(i,Nx)/2)*cospi(chebx(j,Ny)/2))*
+                            sin((pi/twist)*cospi(chebx(i,Nx)/2)*cospi(chebx(j,Ny)/2)) for i in 1:Nx+1, j in 1:Ny+1]))
+gUV    = diagm(vec(Float64[+cos((pi/twist)*cospi(chebx(i,Nx)/2)*cospi(chebx(j,Ny)/2))^2 for i in 1:Nx+1, j in 1:Ny+1]))
+gVU    = diagm(vec(Float64[-sin((pi/twist)*cospi(chebx(i,Nx)/2)*cospi(chebx(j,Ny)/2))^2 for i in 1:Nx+1, j in 1:Ny+1]))
+gVV    = diagm(vec(Float64[+cos((pi/twist)*cospi(chebx(i,Nx)/2)*cospi(chebx(j,Ny)/2))*
+                            sin((pi/twist)*cospi(chebx(i,Nx)/2)*cospi(chebx(j,Ny)/2)) for i in 1:Nx+1, j in 1:Ny+1])) 
+
+L      = WUV*(gUU*DU*DU + gUV*DU*DV + gVU*DV*DU + gVV*DV*DV) 
+B      = bpH2L
+u      = (L + B)\ b  
+uL2H   = shapeL2H(u, Nx, Ny)
+
+using PyPlot
+plot(uL2H[end, :])
+plot(uL2H[1, :])
+#show()
+
+@show cond(L + B)
+@show sort(abs.(eigvals(L + B)))[1:3]
+
+#drawmultipatch(Dict([1,1]=> Patch([1,1], uL2H)), "test-action-operator")
