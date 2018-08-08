68bd169f77c4edfa3ce99b02c2ac55559698d526
Soham Mukherjee <smukherjee@perimeterinstitute.ca>
2018-08-08 17:29:28 -0400

    Updating summation-by-parts test to introduce the directional boundary operator

--- test/OperatorTest.jl
+++ test/OperatorTest.jl
@@ -16,17 +16,27 @@ W  = [ 0.5  0.0  0.0  0.0;
        0.0  0.0  1.0  0.0;
        0.0  0.0  0.0  0.5]
 
-u = [x^5 for x in linspace(-1, 1, 4)]
+B  = [-1.0  0.0  0.0  0.0;
+       0.0  0.0  0.0  0.0;
+       0.0  0.0  0.0  0.0;
+       0.0  0.0  0.0  1.0] 
+
+u = [x^2 for x in linspace(-1, 1, 4)]
 v = [x^3 for x in linspace(-1, 1, 4)]
-@test u'*W*D*v + v'*W*D*u == u[end]*v[end] - u[1]*v[1]
+@test W*D + (W*D)' == B
+@test u'*W*D*v + u'*(W*D)'*v == u'*B*v
 
 # Now test the spectral discretization operators 
 @testset "Summation-by-Parts" begin
     @testset "N = $N" for N in 1:6
         D = Float64[chebd(i,j, N) for i in 1:N+1, j in 1:N+1]
         W = diagm(Float64[chebw(i, N) for i in 1:N+1])
+        B = zeros(N+1, N+1)
+        B[1,1] = 1
+        B[end, end] = -1
         u = [x^2 for x in chebgrid(N)]
         v = [x^3 for x in chebgrid(N)]
-        @test u'*W*D*v + v'*W*D*u ≈ -(u[end]*v[end] - u[1]*v[1])
+        @test u'*W*D*v + u'*(W*D)'*v ≈ u'*B*v atol=1e-15 
+        @test_broken (W*D) + (W*D)' ≈ B atol=1e-15 
     end
 end;
