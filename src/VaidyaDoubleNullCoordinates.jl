8e1287cf3962e397af27a0564c9ad728365524d9
Soham <soham@Sohams-MacBook-Pro.local>
2018-11-27 13:16:34 -0500

    First commit to Vaidya

---
+++ src/VaidyaDoubleNullCoordinates.jl
@@ -0,0 +1,6 @@
+#--------------------------------------------------------------------
+# Spacetime Discretization methods in Julia
+# Soham 06-2018
+# Double null coordinates for a Vaidya Spacetime
+#--------------------------------------------------------------------
+
