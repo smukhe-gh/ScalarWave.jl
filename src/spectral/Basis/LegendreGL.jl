55e6f881225fa5d05f8dc6fcf2f63aa1db89b4bd
Soham Mukherjee <smukherjee@perimeterinstitute.ca>
2019-05-07 10:12:24 -0400

    Refining LegendreGL

--- src/spectral/Basis/LegendreGL.jl
+++ src/spectral/Basis/LegendreGL.jl
@@ -6,16 +6,17 @@
 #--------------------------------------------------------------------
 
 using FastGaussQuadrature
+using Jacobi
 using Memoize
 
 @memoize function nodes(N::Int)::Array{Float64,1}
-    return gausslobatto(N+1)[1]
+    return gausslobatto(N+1)
 end
 
 function collocation(S::GaussLobatto{Tag, N, min, max}, i::Int) where {Tag, N, min, max}
     @assert max > min
     @assert i <= N+1
-    return (nodes(N)[i])*(max-min)/2 + (max + min)/2
+    return (nodes(N)[1][i])*(max-min)/2 + (max + min)/2
 
 end
 
@@ -23,22 +24,20 @@ function derivative(S::GaussLobatto{Tag, N, min, max}, i::Int, j::Int) where {Ta
     @assert max > min
     @assert i <= N+1
     @assert j <= N+1
+    J = (max - min)/2
     if i == j == 1
-        return (1/4)*N*(N+1)
+        return (1/4)*N*(N+1)*J
     elseif i == j == N+1
-        return -(1/4)*N*(N+1)
+        return -(1/4)*N*(N+1)*J
     elseif i == j && j < N+1
         return 0
     else
         xi = collocation(S, i)
         xj = collocation(S, j)
-        return (legendre(N, xi)/legendre(N, xj))*(xi - xj)
+        return ((legendre(N, xi)/(legendre(N, xj))*(xi - xj)))*J
     end
 end
 
 function quadrature(S::GaussLobatto{Tag, N, min, max}, i::Int) where {Tag, N, min, max}
-    @assert max > min
-    @assert i <= N+1
-    xj = collocation(S, j)
-    return 2/(N*(N+1)*(legendre(N, xj))^2)
+    return (nodes(N)[2][i])*(max-min)/2 
 end
